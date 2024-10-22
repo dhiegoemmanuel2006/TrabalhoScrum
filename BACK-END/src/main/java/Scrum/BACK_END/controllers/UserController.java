fbeab51bb1f215569ddb2b0ca76cb0304583afec
dhiegoemmanuel2006 <dhiegoemmanuelsouza@gmail.com>
2024-10-22 12:21:44 -0300

    ADD: Controlador dos ENDPOINT

---
+++ BACK-END/src/main/java/Scrum/BACK_END/controllers/UserController.java
@@ -0,0 +1,32 @@
+package Scrum.BACK_END.controllers;
+
+import Scrum.BACK_END.entities.User;
+import Scrum.BACK_END.service.UserService;
+import Scrum.BACK_END.DTOS.UserDTO;
+import Scrum.BACK_END.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private UserService services;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user) throws Exception {
+        System.out.println("Novo usuário cadastrado!");
+        return services.createUser(user);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() throws Exception {
+        return services.getAllUsers();
+    }
+
+}
