c070ca862ead54fdb3b0277ff919faf4e362dd10
dhiegoemmanuel2006 <dhiegoemmanuelsouza@gmail.com>
2024-10-22 19:31:48 -0300

    UPDATE: Melhoria no Repository para adicionar os métodos Find by Email e Find by Phone

--- BACK-END/src/main/java/Scrum/BACK_END/repositories/UserRepository.java
+++ BACK-END/src/main/java/Scrum/BACK_END/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import Scrum.BACK_END.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
